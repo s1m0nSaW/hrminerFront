7f002ecde93d66514e06a5a9e3480cd648c4b891
s1m0nSaW <simonsaw777@gmail.com>
2023-04-12 09:59:23 +0900

    axios baseURL to server:3000

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://45.9.43.102:5000'
+    baseURL: 'http://server:5000'
 });
 
 instance.interceptors.request.use((config) => {
